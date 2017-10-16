cc0f858243f69c21748d0b6374645f67fd7945d0
段逸轩 <31588755+XiaoXuanDi@users.noreply.github.com>
2017-10-16 17:32:49 +0800

    JS文件

---
+++ scripts/global.js
@@ -0,0 +1,460 @@
+//页面加载后执行一个函数，该函数用于有多个函数要执行的情况。
+function addLoadEvent(func){
+	var oldonload = window.onload;
+	if (typeof window.onload!='function') {
+		window.onload=func;
+	}else{
+		window.onload=function(){
+			oldonload();
+			func();
+		}
+	}
+}
+
+//想要在某个元素的背后插入新元素  JS提供insertBefore方法但没有提供insertAfter!!
+function insertAfter(newElement,targetElement){
+	var parent=targetElement.parentNode;
+	if (parent.lastChild==targetElement) {
+		parent.appendChild(newElement);
+	}else{
+		parent.insertBefore(newElement,targetElement.nextSibling);
+	}
+}
+
+//为某个元素添加类，value是想要添加的类名
+function addClass(element,value){
+	if (!element.className) {
+		element.className=value;
+	}else{
+		newClassName=element.className;
+		newClassName+=" ";
+		newClassName+= value;
+		element.className=newClassName;
+	}
+}
+
+//hightlightPage、moveElement、prepareSlideshow三个方法用于index.html
+
+//该函数的作用是取得导航表中的所有链接，循环遍历后，若当前链接的URL和当前网页的URL相匹配，则为它添加here类，同时给每个页面的body添加专属的id属性。
+ function hightlightPage(){
+ 	if (!document.getElementsByTagName) return false;
+ 	if (!document.getElementById) return false;
+ 	var headers=document.getElementsByTagName('header');
+ 	if (headers.length==0) return false;
+ 	var navs=headers[0].getElementsByTagName('nav');
+ 	if(navs.length==0) return false;
+ 	var links=navs[0].getElementsByTagName('a');
+ 	var linkurl;
+ 	for(var i=0;i<links.length;i++){
+ 		linkurl=links[i].getAttribute("href");						//获取当前链接的url
+ 		if (window.location.href.indexOf(linkurl)!=-1) {			//获取当前页面的url  
+ 			links[i].className="here";								//若匹配，则添加here类
+ 			var linktext= links[i].lastChild.nodeValue.toLowerCase();
+ 			document.body.setAttribute("id",linktext);				//这样子index.html就有值为"home"的id,about.html就会有值为"about"的id..以此类推。
+ 		}
+ 	}
+ }
+
+
+//该函数作用是把元素移动到想要的位置，并且通过interval参数来控制时间间隔。
+function moveElement(elementID,final_x,final_y,interval){
+	if (!document.getElementById) return false;
+	if (!document.getElementById(elementID)) return false;
+	var elem=document.getElementById(elementID);
+	if (elem.movement) {
+		clearTimeout(elem.movement);
+	}
+	if (!elem.style.left) {
+		elem.style.left="0px";
+	}
+	if (!elem.style.top) {
+		elem.style.top="0px";
+	}
+	var xpos=parseInt(elem.style.left);
+	var ypos=parseInt(elem.style.top);
+	if (xpos==final_x&&ypos==final_y) {
+		return true;
+	}
+	if (xpos<final_x) {
+		var dist=Math.ceil((final_x-xpos)/10);
+		xpos=xpos+dist;
+	}
+	if (xpos>final_x) {
+		var dist=Math.ceil((xpos-final_x)/10);
+		xpos=xpos-dist;
+	}
+	if (ypos<final_y) {
+		var dist=Math.ceil((final_y-ypos)/10);
+		ypos=ypos+dist;
+	}
+	if (ypos>final_y) {
+		var dist=Math.ceil((ypos-final_y)/10);
+		ypos=ypos-dist;
+	}
+	elem.style.left=xpos+"px";
+	elem.style.top=ypos+"px";
+	var repeat="moveElement('"+elementID+"',"+final_x+","+final_y+","+interval+")";
+	elem.movement=setTimeout(repeat,interval);
+}
+
+//该函数作用是根据鼠标所在的链接来移动preview元素，即是幻灯片效果。
+function prepareSlideshow(){
+	if (!document.getElementById) return false;
+	if (!document.getElementsByTagName) return false;
+	if (!document.getElementById("intro")) return false;
+	var intro=document.getElementById("intro");
+	var slideshow=document.createElement("div");
+	slideshow.setAttribute("id","slideshow");
+/*	
+	var frame=document.createElement("img");
+	frame.setAttribute("src","images/frame.gif");
+	frame.setAttribute("alt","");
+	frame.setAttribute("id","frame");
+	slideshow.appendChild(frame);
+*/
+	var preview=document.createElement("img");
+	preview.setAttribute("src","images/slideshow.gif");
+	preview.setAttribute("alt","a glimpse of what awaits you");
+	preview.setAttribute("id","preview");
+	slideshow.appendChild(preview);
+	insertAfter(slideshow,intro);
+	var links=document.getElementsByTagName("a");
+	var destination;
+	for (var i = 0; i < links.length; i++) {
+		links[i].onmouseover=function(){
+			destination=this.getAttribute("href");
+			if (destination.indexOf("index.html")!=-1) {
+				moveElement("preview",0,0,5);
+			}
+			if (destination.indexOf("about.html")!=-1) {
+				moveElement("preview",-150,0,5);
+			}
+			if (destination.indexOf("photos.html")!=-1) {
+				moveElement("preview",-300,0,5);
+			}
+			if (destination.indexOf("live.html")!=-1) {
+				moveElement("preview",-450,0,5);
+			}
+			if (destination.indexOf("contact.html")!=-1) {
+				moveElement("preview",-600,0,5);
+			}
+		}
+	}
+}
+
+
+//showSection、prepareInternalnav两个方法用于about.html
+
+//作用是通过判断id是否一致来隐藏和显示section里面的内容。prepareInternalnav方法中设置了默认全都隐藏。
+function showSection(id) {
+  var sections = document.getElementsByTagName("section");
+  for (var i=0; i<sections.length; i++ ) {
+    if (sections[i].getAttribute("id") != id) {
+      sections[i].style.display = "none";
+    } else {
+      sections[i].style.display = "block";
+    }
+  }
+}
+
+
+//通过点击事件来调用showSection方法，getAttribute("href").split("#")[1]作用是提取"#"后面的字符串。在这函数里是用来传值做比较。   若是结尾是[0]则是提取"#"前面的字符串。
+function prepareInternalnav(){
+	if (!document.getElementById) return false;
+	if (!document.getElementsByTagName) return false;
+	var articles=document.getElementsByTagName("article");
+	if (articles.length==0) return false;
+	var navs=articles[0].getElementsByTagName("nav");
+	if (navs.length==0) return false;
+	var nav=navs[0];
+	var links=nav.getElementsByTagName("a");
+	for (var i = 0; i < links.length; i++) {
+		var sectionId=links[i].getAttribute("href").split("#")[1];
+		if (!document.getElementById(sectionId)) continue;
+		document.getElementById(sectionId).style.display="none";
+		links[i].destination=sectionId;
+		links[i].onclick=function(){
+			showSection(this.destination);
+			return false;
+		}
+	}
+}
+
+//showPic、preparePlaceholder、prepareGallery三个函数用于photos.html
+
+//通过被prepareGallery调用来切换图片和更改描述图片的title值，此处获得的id为“placeholder”是preparePlaceholder创建的img元素
+function showPic(whichpic){
+	if (!document.getElementById) return false;
+	var source=whichpic.getAttribute("href");
+	var placeholder=document.getElementById("placeholder");
+	placeholder.setAttribute("src",source);
+	if (!document.getElementById("description")) return false;
+	if (whichpic.getAttribute("title")) {
+		var text=whichpic.getAttribute("title");
+	}else{
+		var text="";
+	}
+	var description=document.getElementById("description");
+	if (description.firstChild.nodeType==3) {
+		description.firstChild.nodeValue=text;
+	}
+	return false;
+}
+
+
+//创建一个img图片和p片段描述
+function preparePlaceholder(){
+	if (!document.getElementById) return false;
+	if (!document.createTextNode) return false;
+	if (!document.createElement) return false;
+	if (!document.getElementById("imagegallery")) return false;
+	var placeholder=document.createElement("img");
+	placeholder.setAttribute("id","placeholder");
+	placeholder.setAttribute("src","images/placeholder.gif");
+	placeholder.setAttribute("alt","my image gallery");
+	var description=document.createElement("p");
+	description.setAttribute("id","description");
+	var desctext=document.createTextNode("choose an image");
+	description.appendChild(desctext);
+	var gallery=document.getElementById("imagegallery");
+	insertAfter(description,gallery);
+	insertAfter(placeholder,description);
+}
+
+
+//获得id为imagegallery中的a链接 然后通过点击事件来切换图片，通过showPic函数切换
+function prepareGallery(){
+	if (!document.getElementById) return false;
+	if (!document.getElementsByTagName) return false;
+	if (!document.getElementById("imagegallery")) return false;
+	var gallery=document.getElementById("imagegallery");
+	var links=gallery.getElementsByTagName("a");
+	for (var i = 0; i < links.length; i++) {
+		links[i].onclick=function(){
+			return showPic(this);
+		}
+	}
+}
+
+
+//stripeTables、highlightRows、displayAbbreviations三个方法用于live.html
+
+//这个函数的作用是分出表格的奇偶行，以便于css设置样式
+function stripeTables(){
+	if (!document.getElementsByTagName) return false;
+	var tables=document.getElementsByTagName("table");
+	for (var i = 0; i < tables.length; i++) {
+		var odd=false;
+		var rows=tables[i].getElementsByTagName("tr");
+		for (var j = 0; j < rows.length; j++) {
+			if (odd==true) {
+				addClass(rows[j],"odd");
+				odd=false;
+			}else{
+				odd=true;
+			}
+		}
+	}
+}
+
+//该方法是通过onmouseover来添加类，onmouseout来还原。也是用于css，以实现鼠标放上去和离开的动画效果。
+function highlightRows(){
+	if (!document.getElementsByTagName) return false;
+	var rows=document.getElementsByTagName("tr");
+	for (var i = 0; i < rows.length; i++) {
+		rows[i].oldClassName=rows[i].className;
+		rows[i].onmouseover=function(){
+			addClass(this,"highlight");
+		}
+		rows[i].onmouseout=function(){
+			this.className=this.oldClassName;
+		}
+	}
+}
+
+//该函数作用是将表格里面的abbr的缩写内容提取出来并且显示出来
+function displayAbbreviations(){
+	if (!document.getElementsByTagName||!document.createTextNode||!document.createElement) return false;
+	var abbreviations=document.getElementsByTagName("abbr");
+	if (abbreviations.length<1) return false;
+	var defs =new Array();
+	for (var i = 0; i < abbreviations.length; i++) {
+		var current_abbr=abbreviations[i];
+		if (current_abbr.childNodes.length<1) continue;
+		var definition=current_abbr.getAttribute("title");
+		var key=current_abbr.lastChild.nodeValue;
+		defs[key]=definition;
+	}
+	var dlist=document.createElement("dl");
+	for(key in defs){
+		var definition=defs[key];
+		var dtitle=document.createElement("dt");
+		var dtitle_text=document.createTextNode(key);
+		dtitle.appendChild(dtitle_text);
+		var ddesc=document.createElement("dd");
+		var ddesc_text=document.createTextNode(definition);
+		ddesc.appendChild(ddesc_text);
+		dlist.appendChild(dtitle);
+		dlist.appendChild(ddesc);
+	}
+	if (dlist.childNodes.length<1) return false;
+	var header=document.createElement("h3");
+	var header_text=document.createTextNode("Abbreviations");
+	header.appendChild(header_text);
+	var articles=document.getElementsByTagName("article");
+	if (articles.length==0) return false;
+	var container=articles[0];
+	container.appendChild(header);
+	container.appendChild(dlist);
+}
+
+
+//余下函数都用于contact.html
+
+//该方法作用是当点击label标签时，关联的表单字段会获得焦点。
+function focusLabels(){
+	if (!document.getElementsByTagName) return false;	
+	var labels=document.getElementsByTagName("label");
+	for (var i = 0; i < labels.length; i++) {
+		if (!labels[i].getAttribute("for")) continue;
+		labels[i].onclick=function(){
+			var id=this.getAttribute("for");
+			if (!document.getElementById) return false;
+			var element=document.getElementById(id);
+			element.focus();
+		}
+	}
+}
+
+
+function resetFields(whichform){
+	if (Modernizr.input.placeholder) return;
+	for (var i = 0; i < whichform.length; i++) {
+		var element=whichform.element[i];
+		if (element.type=="submit") continue;
+		var check=element.placeholder||element.getAttribute('placeholder');
+		if (!check) continue;
+		element.onfocus=function(){
+			var text=this.placeholder||this.getAttribute('placeholder');
+			if (this.value==text) {
+				this.className='';
+				this.value="";
+			}
+		}
+		element.onblur=function(){
+			if (this.value=="") {
+				this.className='placeholder';
+				this.value=this.placeholder||this.getAttribute('placeholder');
+			}
+		}
+		element.onblur();
+	}
+}
+
+function isFilled(field){
+	if (field.value.replace(' ','').length==0) return false;
+	var placeholder=field.placeholder||field.getAttribute('placeholder');
+	return (field.value!=placeholder);
+}
+
+function isEmail(field){
+	return (field.value.indexOf("@")!=-1&&field.value.indexOf(".")!=-1);
+}
+
+function validateForm(whichform){
+	for (var i = 0; i < whichform.elements.length; i++) {
+		var element=whichform.elements[i];
+		if (element.required=='required') {
+			if (!isFilled(element)) {
+				alert("Please fill in the "+element.name+" field.");
+				return false;
+			}
+		}
+		if (element.type=='email') {
+			if (!isEmail(element)) {
+				alert("The "+element.name+" field must be a valid email address.")
+				return false;
+			}
+		}
+	}
+	return true;
+}
+
+function prepareForms(){
+	for (var i = 0; i < document.forms.length; i++) {
+		var thisform=document.forms[i];
+		resetFields(thisform);
+		thisform.onsubmit=function(){
+			if (!validateForm(this)) return false;
+			var article=document.getElementsByTagName('article')[0];
+			if (submitFormWithAjax(this,article)) return false;
+			return true;
+		}
+	}
+}
+
+function getHTTPObject(){
+	if (typeof XMLHttpRequest=="undefined")
+		XMLHttpRequest=function(){
+			try{ return new ActiveXObject("Msxml2.XMLHTTP.6.0"); }
+				catch(e){}
+			try{ return new ActiveXObject("Msxml2.XMLHTTP.3.0"); }
+				catch(e){}
+			try{ return new ActiveXObject("Msxml2.XMLHTTP"); }
+				catch(e){}
+			return false;
+	}
+	return new XMLHttpRequest();
+}
+
+function displayAjaxLoading(element){
+	while(element.hasChildNodes()){
+		element.removeChild(element.lastChild);
+	}
+	var content=document.createElement("img");
+	content.setAttribute("src","images/loading.gif");
+	content.setAttribute("alt","Loading...");
+	element.appendChild(content);
+}
+
+function submitFormWithAjax(whichform,thetarget){
+	var request=getHTTPObject();
+	if (!request) return false;
+	displayAjaxLoading(thetarget);
+	var dataParts=[];
+	var element;
+	for (var i = 0; i < whichform.elements.length; i++) {
+		element=whichform.elements[i];
+		dataParts[i]=element.name+'='+encodeURIComponent(element.value);
+	}
+	var data = dataParts.join('&');
+	request.open('POST',whichform.getAttribute("action"),true);
+	request.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+	request.onreadystatechange=function(){
+		if (request.readyState==4) {
+			if (request.status==200||request.status==0) {
+				var matches=request.responseText.match(/<article>([\s\S]+)<\/article>/);
+				if (matches.length>0) {
+					thetarget.innerHTML=matches[1];
+				}else{
+					thetarget.innerHTML='<p>Oops,there was an error.Sorry.</p>';
+				}
+			}else{
+				thetarget.innerHTML='<p>'+request.statusText+'</p>'
+			}
+		}
+	};
+	request.send(data);
+	return true;
+};
+
+addLoadEvent(hightlightPage);
+addLoadEvent(prepareSlideshow);
+addLoadEvent(prepareInternalnav);
+addLoadEvent(preparePlaceholder);
+addLoadEvent(prepareGallery);
+addLoadEvent(stripeTables);
+addLoadEvent(highlightRows);
+addLoadEvent(displayAbbreviations);
+addLoadEvent(focusLabels);
+addLoadEvent(prepareForms);
